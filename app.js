f8d90c47c03bc2bdb2b512c0952935867d959034
Elisee Desmarest <elisee.desmarest@gmail.com>
2026-03-04 11:33:03 +0100

    authentification user

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ const AuthRouter = require("./routes/auth");
 
 app.use(express.json());
 
-app.use("/", indexRouter);
-app.use("/auth", AuthRouter);
+app.use("/api", indexRouter);
+app.use("/api/auth", AuthRouter);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/User");
 
@@ -44,6 +46,52 @@ router.post("/register", async (req, res) => {
             });
         }
 
+        return res.status(500).json({
+            error: {
+                code: "INTERNAL_SERVER_ERROR",
+                message: "An unexpected error occurred",
+            },
+        });
+    }
+});
+
+router.post("/login", async (req, res) => {
+    const {email, password} = req.body;
+
+    try {
+        const user = await User.findOne({email: email});
+        if (!user) {
+            return res.status(401).json({
+                error: {
+                    code: "INVALID_CREDENTIALS",
+                    message: "Invalid credentials",
+                },
+            });
+        }
+
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) {
+            return res.status(401).json({
+                error: {
+                    code: "INVALID_CREDENTIALS",
+                    message: "Invalid credentials",
+                },
+            });
+        }
+
+        const accessToken = jwt.sign({
+            userId: user._id,
+        }, process.env.JWT_SECRET, {
+            expiresIn: "15m",
+        });
+
+        return res.status(200).json({
+            success: true,
+            message: "Login successful",
+            accessToken: accessToken,
+        });
+
+    } catch (err) {
         return res.status(500).json({
             error: {
                 code: "INTERNAL_SERVER_ERROR",
