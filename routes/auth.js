69da49a017b76170fcd3cec903a393ccbc164143
Elisee Desmarest <elisee.desmarest@gmail.com>
2026-03-05 12:27:10 +0100

    edit post

--- routes/auth.js
+++ routes/auth.js
@@ -89,7 +89,7 @@ router.post("/login", async (req, res) => {
 
         return res.status(200).json({
             success: true,
-            message: "Login successful",
+            message: "User authenticated successfully",
             accessToken: accessToken,
         });
 
